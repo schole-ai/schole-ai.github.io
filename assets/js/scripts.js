43159aa8eb5d047785cce2b41b0f4aaad81b8aa9
Vinitra Swamy <vinitras@gmail.com>
2024-07-10 08:54:31 -0700

    updating backend

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,31 +29,36 @@ jQuery(document).ready(function() {
 	$('.success-message').hide();
 	$('.error-message').hide();
 	
-	$('.subscribe form').submit(function(e) {
-		e.preventDefault();
-	    var postdata = $('.subscribe form').serialize();
-	    $.ajax({
-	        type: 'POST',
-	        url: 'assets/subscribe.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-	            if(json.valid == 0) {
-	                $('.success-message').hide();
-	                $('.error-message').hide();
-	                $('.error-message').html(json.message);
-	                $('.error-message').fadeIn();
-	            }
-	            else {
-	                $('.error-message').hide();
-	                $('.success-message').hide();
-	                $('.subscribe form').hide();
-	                $('.success-message').html(json.message);
-	                $('.success-message').fadeIn();
-	            }
-	        }
-	    });
-	});
+	jQuery(document).ready(function() {
+
+		// Other initializations
+	
+		$('.subscribe form').submit(function(e) {
+			e.preventDefault();
+			var postdata = $('.subscribe form').serialize();
+			$.ajax({
+				type: 'POST',
+				url: 'assets/subscribe.php',
+				data: postdata,
+				dataType: 'json',
+				success: function(json) {
+					if(json.valid === 0) {
+						$('.success-message').hide();
+						$('.error-message').hide();
+						$('.error-message').html(json.message);
+						$('.error-message').fadeIn();
+					} else {
+						$('.error-message').hide();
+						$('.success-message').hide();
+						$('.subscribe form').hide();
+						$('.success-message').html(json.message);
+						$('.success-message').fadeIn();
+					}
+				}
+			});
+		});
+	
+	});	
     
 });
 
